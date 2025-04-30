7e5bee0a6752d04be8d8c2384a28b3a8e5c1d895
Josefadarrouymarin <165593890+Josefadarrouymarin@users.noreply.github.com>
2025-04-30 15:21:15 -0400

    Haciendo de nuevo lab 5 :0

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,27 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts "Creating users..."
+users = 10.times.map do |i|
+  User.create!(
+    email: "user#{i}@example.com",
+    first_name: "First#{i}",
+    last_name: "Last#{i}"
+  )
+end
+
+puts "Creating chats..."
+chats = 10.times.map do
+  Chat.create!(
+    sender_id: users.sample.id,
+    receiver_id: users.sample.id
+  )
+end
+
+puts "Creating messages..."
+10.times do
+  Message.create!(
+    chat_id: chats.sample.id,
+    user_id: users.sample.id,
+    body: "Este es un mensaje de prueba"
+  )
+end
+
+puts "Done seeding!"
